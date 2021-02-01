8ef3991b7bd61f4d654d0c6305c038b9c6ede6a4
Kushagra Gupta <guptakushagra15.10@gmail.com>
2021-02-01 14:50:23 +0530

    🔥 Remove Home.module.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-// import Head from "next/head";
 import Footer from "../shared/components/footer";
 import Navbar from "../shared/components/navbar";
-// import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
