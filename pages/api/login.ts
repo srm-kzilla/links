379a95eb330623858c7065ca1a7f59c9343fcf49
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-02-06 16:18:14 +0530

    ✨ Added auth

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,11 +3,28 @@
 import { MongoClient } from 'mongodb';
 import { getDbClient } from "../../util/mongodb";
 import { NextApiRequest, NextApiResponse } from "next"
+import jwt from 'jsonwebtoken'
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     var link = req.body.link;
     var email = req.body.email;
     const dbClient: MongoClient = await getDbClient();
+    if (!req.body){
+        res.statusCode = 404
+        res.end('Error')
+        return
+    }
+
+    const { username, password } = req.body;
+    
+    res.json({
+        token: jwt.sign({
+            username,
+            admin:
+        })
+        }
+    )
+    
     // Get the documents collection
     const collection = dbClient.db('links').collection('link');
     // Insert a document
