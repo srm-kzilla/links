91a5369fa24026d8e1914a419954bf84381cb468
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-05-06 02:50:56 +0530

    ✨  Verify recaptcha middleware

--- pages/api/[[...routes]].ts
+++ pages/api/[[...routes]].ts
@@ -7,15 +7,15 @@ import profileRoutes from "../../api/profile/profile.routes";
 import { validateUser } from "../../api/middlewares/verifyJWT.middleware";
 import { onError, onNotFound } from "../../api/error/error.controller";
 import { rateLimiter } from "../../api/middlewares/rateLimiter";
-
+import { verifyRecaptcha } from "../../api/middlewares/verifyRecaptcha";
 const indexRouter = nc<NextApiRequest, NextApiResponse>({
   onNoMatch: onNotFound,
   onError: onError,
 });
 indexRouter.use(rateLimiter);
-indexRouter.use("/api/v1/auth", authRoutes);
-indexRouter.use("/api/v1/links", validateUser, linksRoutes);
-indexRouter.use("/api/v1/public/links", publicRoutes);
-indexRouter.use("/api/v1/profile", profileRoutes);
+indexRouter.use("/api/v1/auth", verifyRecaptcha, authRoutes);
+indexRouter.use("/api/v1/links", verifyRecaptcha, validateUser, linksRoutes);
+indexRouter.use("/api/v1/public/links", verifyRecaptcha, publicRoutes);
+indexRouter.use("/api/v1/profile", verifyRecaptcha, profileRoutes);
 
 export default indexRouter;
