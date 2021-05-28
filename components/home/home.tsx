3b786e5f0b6dac9e0f0dbef494f7255ebb10b3e2
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-05-28 18:13:26 +0530

    💄  Change font style

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -3,9 +3,9 @@ import { HeroLanding } from "../../assets/icons";
 export default function HomeComponent(): JSX.Element {
   return (
     <>
-      <div className="bg-white flex flex-col font-sans pt-20 min-h-screen px-0 sm:px-8">
+      <div className="bg-white flex flex-col font-sans pt-20 md:pt-32 min-h-screen px-0 sm:px-8">
         <div className="grid grid-cols-0 xl:grid-cols-3">
-          <h1 className="col-span-2 uppercase text-5xl sm:text-8xl lg:text-xxl customGradientInverse font-bold leading-none tracking-wide mb-2 mx-auto md:ml-0">
+          <h1 className="col-span-2 text-5xl sm:text-7xl md:text-9xl customGradientInverse font-bold leading-none tracking-wide mb-2 mx-auto lg:ml-24">
             One link...
           </h1>
           <div></div>
@@ -15,10 +15,10 @@ export default function HomeComponent(): JSX.Element {
             <HeroLanding />
           </div>
           <div className="md:col-span-1 my-auto">
-            <h2 className="uppercase text-3xl sm:text-6xl lg:text-7xl text-center text-lightblue text-secondary tracking-wider mb-6">
+            <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold text-lightblue text-secondary tracking-wider mb-6">
               For all your links
             </h2>
-            <p className="text-gray-600 text-md sm:text-lg lg:text-2xl text-center leading-relaxed px-10 xl:px-28 2xl:px-40">
+            <p className="text-gray-600 text-md sm:text-xl lg:text-2xl text-center leading-relaxed px-10 xl:px-28 2xl:px-40">
               Lorem ipsum dolor sit amet, consectetur adipiscing. Vestibulum
               rutrum metus at enim congue scelerisque. Sed suscipit metu non
               iaculis semper consectetur adipiscing elit.
@@ -26,7 +26,7 @@ export default function HomeComponent(): JSX.Element {
             <div className=" flex items-center justify-center pb-4">
               <a
                 href="/signup"
-                className="bg-lightblue flex items-center justify-center rounded hover:bg-opacity-90 shadow-md focus:outline-none p-3 uppercase text-lg font-bold text-white mt-9 w-10/12 sm:w-2/5"
+                className="bg-lightblue flex items-center justify-center rounded hover:bg-opacity-90 shadow-md focus:outline-none p-3 text-lg font-bold text-white mt-9 w-10/12 sm:w-2/5"
               >
                 Get Started
               </a>
