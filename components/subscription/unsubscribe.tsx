c6223a529d48e38762dd2f253763fa9daae98b9a
Kushagra Gupta <guptakushagra15.10@gmail.com>
2021-05-30 20:43:26 +0530

    💄 Add lottieicons for unsubscribe, verifyemail

--- components/subscription/unsubscribe.tsx
+++ components/subscription/unsubscribe.tsx
@@ -1,13 +1,37 @@
-import { UnsubscribeImage } from "../../assets/icons";
+import Lottie from "react-lottie";
+import animationData from "../../public/lotties/unsubscribe.json";
+
 export default function UnsubscribeComponent(): JSX.Element {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
   return (
     <>
-      <div>
-        <div className="absolute text-3xl lg:text-6xl top-20 left-4 gradientHeaderHollow">
-          <h1>Sorry to see you go :(</h1>
-        </div>
-        <div className="relative flex items-center min-h-screen justify-center my-auto">
-          <UnsubscribeImage />
+      <div className="flex h-screen">
+        <div className="m-auto w-full">
+          <div className="flex flex-col items-center justify-center p-4">
+            <Lottie
+              options={defaultOptions}
+              height={150}
+              width={150}
+            />
+            <h1 className="font-bold text-xl sm:text-3xl text-center mt-4">Sad to see you go!</h1>
+          </div>
+          <p className="w-full text-center mt-8 text-md sm:text-lg px-1">
+          You have been unsubscribed from the mailing list
+          </p>
+          <div className="text-center">
+            <a href="/">
+              <button className="bg-lightblue focus:outline-none hover:bg-opacity-90 text-darkgray w-2/3 sm:w-1/5 text-md shadow-lg font-extrabold py-3 px-4 my-10 rounded">
+                TAKE ME HOME
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </>
--- components/verifyEmail/verifyemail.tsx
+++ components/verifyEmail/verifyemail.tsx
@@ -1,14 +1,29 @@
 import { setCookie } from "nookies";
+import Lottie from "react-lottie";
+
+import animationData from "../../public/lotties/success.json";
 
 export default function VerifyEmailComponent({ authTokenProp }): JSX.Element {
   setCookie(null, "authToken", authTokenProp);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
   return (
     <>
-      <div className="flex w-screen h-screen">
+      <div className="flex h-screen">
         <div className="m-auto w-full">
           <div className="flex flex-col items-center justify-center p-4">
-              <h1 className="text-7xl sm:text-9xl my-2">✔️</h1>
-              <h1 className="font-bold text-xl sm:text-3xl text-center">Your Email is now verified!</h1>
+            <Lottie
+              options={defaultOptions}
+              height={300}
+              width={300}
+            />
+            <h1 className="font-bold text-xl sm:text-3xl text-center">Your Email is now verified!</h1>
           </div>
           <p className="w-full text-center mt-8 text-md sm:text-lg">
             You can now continue to add your links in your dashboard! 🥳
