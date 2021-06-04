5a3c622bbf3e92f2985e29300116083db23f5f12
Kushagra Gupta <60519359+KG-1510@users.noreply.github.com>
2021-06-04 17:46:21 +0530

    💬 Update base URL constant

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
 export const authRoutes: string[] = ["/dashboard", "/profile"]
-export const baseUrl: string = "http://localhost:3000/"
+export const baseUrl: string = "https://kzilla-links.herokuapp.com/"
 export const kzillaxyz: string = "https://kzilla.xyz/analytics/"
 export const kzillaxyzdomain: string = "https://kzilla.xyz/"
 export const kzillaxyzclicks: string = "https://kzilla.xyz/api/v1/analytics/"
