84e5597c5a19a9cd8e54199311ee8aa3a37ceb30
Kushagra Gupta <guptakushagra15.10@gmail.com>
2021-06-04 17:09:31 +0530

    🐛 Fix name casing

--- store/profileImageContext.tsx
+++ store/profileImageContext.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
 export const ImageContext = React.createContext({
-    fileBlob: String,
-    setFileBlob: (_fileBlob:String) => {}
+    fileBlob: "",
+    setFileBlob: (_fileBlob) => {}
 });
 
 const ImageContextProvider: React.FC = (props): JSX.Element => {
-    const [fileBlob, setFileBlob] = useState<String>();
+    const [fileBlob, setFileBlob] = useState<string>();
     return (
         <ImageContext.Provider
             value={{fileBlob, setFileBlob }}
