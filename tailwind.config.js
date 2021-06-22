99569e6144bdf064eef20f567b1c7218e222b517
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-06-22 17:08:58 +0530

    💄  Develop vector for herolanding

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       fontFamily: {
         WorkSans: ["Work Sans", "sans-serif"],
         NotoSans: ["Noto Sans", "sans-serif"],
+        Inter: ["Inter", "sans-serif"],
       },
       width: {
         custom: "21.66666%",
@@ -20,12 +21,13 @@ module.exports = {
         offwhite: "#F4FBF7",
         lightblue: "#56CCF2",
         darkgray: "#333333",
-        lightgray: "#F2F2F2",
+        lightestgray: "#F2F2F2",
         lightgraycustom: "#A7A6A6",
         buttongray: "#4F4F4F",
         statusRed: "#EB5757",
         statusGreen: "#6FCF97",
         customGreen: "#32B583",
+        lightestGreen: "#F1FAF5",
       },
       boxShadow: {
         custom: "1px 1px 6px rgba(0, 0, 0, 0.25)",
@@ -35,11 +37,11 @@ module.exports = {
       },
       inset: {
         addButton: "70%",
-        '37': "9.5rem",
+        37: "9.5rem",
       },
       minHeight: {
-        'custom': "90vh",
-      }
+        custom: "90vh",
+      },
     },
   },
   variants: {
