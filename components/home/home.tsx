4e312803fd3036a4056fbf16a4b2b7570d349722
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-06-22 17:11:24 +0530

    💄  Redesign homepage

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -4,11 +4,13 @@ import {
   Circle,
   DoubleCircle,
   LoadingAuth,
+  HomeTick,
+  Arrow,
 } from "../../assets/icons";
 import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
 import { useContext, useState } from "react";
-import { postSubscribe } from "../../utils/api";
+import { errorHandler, postSubscribe } from "../../utils/api";
 import { AuthContext } from "../../store/authContext";
 
 export default function HomeComponent(): JSX.Element {
@@ -36,7 +38,7 @@ export default function HomeComponent(): JSX.Element {
         setLoading(false);
       }
     } catch (error) {
-      // Fire and forget
+      errorHandler(error);
     }
   };
   const [loading, setLoading] = useState<boolean>(false);
@@ -44,7 +46,7 @@ export default function HomeComponent(): JSX.Element {
 
   return (
     <>
-      <div className="flex flex-col font-sans min-h-screen pt-4 px-3 sm:px-8 2xl:px-12 relative z-50">
+      <div className="flex flex-col min-h-screen pt-4 px-3 sm:px-8 2xl:px-12 relative z-50">
         <div className="grid grid-cols-2">
           <a
             href="/"
@@ -69,18 +71,14 @@ export default function HomeComponent(): JSX.Element {
           <div className="md:col-span-1 my-auto block md:hidden">
             <HeroLanding />
           </div>
-          <div className="md:col-span-1 my-auto px-5 text-center md:text-left">
-            <h1 className="text-5xl sm:text-7xl md:text-6xl lg:text-8xl 2xl:text-9xl customGradientInverse font-bold tracking-wide">
-              One LINK,
+          <div className="text-gray-600 font-Inter md:col-span-1 my-auto px-5 text-center md:text-left font-bold ">
+            <h1 className="py-3 text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl tracking-wider">
+              Your ultimate
             </h1>
-            <h2 className="mx-auto lg:ml-0 text-3xl sm:text-5xl md:text-4xl lg:text-6xl 2xl:text-7xl font-bold text-lightblue tracking-wide mb-6">
-              For all your links
+            <h2 className="py-3 lg:ml-0 text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl tracking-wider mb-24">
+              URL <span className="text-statusGreen">warehouse</span>
             </h2>
-            <p className="text-gray-600 text-md sm:text-xl lg:text-2xl mb-6">
-              Lorem ipsum dolor sit amet, consectetur adipiscing. Vestibulum
-              rutrum metus at enim congue scelerisque. Sed suscipit metu non
-              iaculis semper consectetur adipiscing elit.
-            </p>
+
             <Formik
               initialValues={initialValues}
               onSubmit={(values) => submitHandler(values)}
@@ -90,11 +88,11 @@ export default function HomeComponent(): JSX.Element {
             >
               {({ errors }) => (
                 <Form>
-                  <div className="grid grid-cols-4 pb-5 md:pb-0">
+                  <div className="grid grid-cols-8 pb-5 md:pb-0">
                     <Field
                       name="email"
                       type="email"
-                      className="col-span-3 p-2 sm:p-5 md:pt-2 lg:pt-5 rounded-lg outline-none focus:outline-none block appearance-none w-full bg-lightgray"
+                      className="col-span-5 p-2 sm:p-7 md:pt-2 lg:pt-5 border-statusGreen border-l-8 focus:outline-none block appearance-none w-full bg-lightestgray"
                       placeholder="abc@example.com"
                     />
 
@@ -104,16 +102,16 @@ export default function HomeComponent(): JSX.Element {
                       className={`${
                         isSubscribed
                           ? "bg-backgroundwhiteinset"
-                          : "bg-lightblue"
-                      } col-span-1 flex items-center justify-center rounded-lg hover:bg-opacity-90 shadow-md focus:outline-none -ml-2 text-sm sm:text-lg md:text-sm lg:text-lg font-bold text-white`}
+                          : "bg-lightestGreen"
+                      } col-span-1 flex items-center justify-center hover:bg-opacity-90 -ml-2 focus:outline-none `}
                     >
                       {loading && (
                         <div className="absolute">
                           <LoadingAuth />
                         </div>
                       )}
-                      <div className={`${loading && "invisible"}`}>
-                        {isSubscribed ? "Subscribed" : "Subscribe"}
+                      <div className={`${loading ? "invisible" : ""}`}>
+                        {isSubscribed ? <HomeTick /> : <Arrow />}
                       </div>
                     </button>
                   </div>
@@ -131,12 +129,12 @@ export default function HomeComponent(): JSX.Element {
           </div>
         </div>
       </div>
-      <div className="absolute right-0 top-32 hidden md:block z-0">
+      {/* <div className="absolute right-0 top-32 hidden md:block z-0">
         <Circle />
       </div>
       <div className="absolute left-0 bottom-16 hidden md:block z-0">
         <DoubleCircle />
-      </div>
+      </div> */}
     </>
   );
 }
