47d31d4a44f63d2d5bf308b648f5cc9dc8447c7a
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-06-22 17:07:08 +0530

    🍱  Add vectors for home page

---
+++ assets/icons/leftdotted.tsx
@@ -0,0 +1,21 @@
+export default function LeftDotted(): JSX.Element {
+  return (
+    <>
+      <svg
+        width={74}
+        height={116}
+        viewBox="0 0 74 116"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2 0V114H74"
+          stroke="#5F5F5F"
+          strokeOpacity="0.16"
+          strokeWidth={3}
+          strokeDasharray="10 10"
+        />
+      </svg>
+    </>
+  );
+}
---
+++ assets/icons/rightdotted.tsx
@@ -0,0 +1,21 @@
+export default function RightDotted(): JSX.Element {
+  return (
+    <>
+      <svg
+        width={101}
+        height={116}
+        viewBox="0 0 101 116"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M99 0V114H3.69549e-06"
+          stroke="#5F5F5F"
+          strokeOpacity="0.16"
+          strokeWidth={3}
+          strokeDasharray="10 10"
+        />
+      </svg>
+    </>
+  );
+}
