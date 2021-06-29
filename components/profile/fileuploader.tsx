ccac63178ad4a96d0c2f74e1aa53e75aea5040bb
Kushagra Gupta <guptakushagra15.10@gmail.com>
2021-06-29 23:28:47 +0530

    📝 Update README.md images

--- components/profile/fileuploader.tsx
+++ components/profile/fileuploader.tsx
@@ -1,52 +1,60 @@
-import React, { useContext } from 'react';
-import { parseCookies } from 'nookies';
+import React, { useContext } from "react";
+import { parseCookies } from "nookies";
 
-import { patchProfilePicture, postProfilePicture } from '../../utils/api';
-import { ImageContext } from '../../store/profileImageContext';
-import { EditPencil } from '../../assets/icons';
+import {
+  patchProfilePicture,
+  postProfilePicture,
+  successHandler,
+} from "../../utils/api";
+import { ImageContext } from "../../store/profileImageContext";
+import { EditPencil } from "../../assets/icons";
 
-export default function FileUploader() :JSX.Element {
-    const { setFileBlob } = useContext(ImageContext);
-    const hiddenFileInput = React.useRef(null);
-    const handleClick = event => {
-        hiddenFileInput.current.click();
-    };
-    const handleChange = event => {
-        const { authToken } = parseCookies();
-        const file = event.target.files[0];
-        if(file) {
-            setFileBlob("loading.gif");
-        }
+export default function FileUploader(): JSX.Element {
+  const { setFileBlob } = useContext(ImageContext);
+  const hiddenFileInput = React.useRef(null);
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
+  const handleChange = (event) => {
+    const { authToken } = parseCookies();
+    const file = event.target.files[0];
+    if (file) {
+      setFileBlob("loading.gif");
+    }
 
-        (async () => {
-            const _res = await patchProfilePicture(authToken);
-            if (_res) {
-                const { fields, url } = await _res.data.postInfo;
-                const formData = new FormData();
-                const formArray: [string, string | File][] = Object.entries({...fields, file});
-                formArray.forEach(([key, value]) => {
-                    formData.append(key, value);
-                });
-                setFileBlob(URL.createObjectURL(event.target.files[0]));
-                await postProfilePicture(url, formData); 
-            }
-        })();
-    };
-    return (
-        <>
-            <i
-                className="absolute bg-white rounded-full p-3 bottom-1 right-0 cursor-pointer shadow-lg"
-                onClick={handleClick}
-            >
-                <EditPencil />
-            </i>
-            <input
-                type="file"
-                ref={hiddenFileInput}
-                onChange={handleChange}
-                className="hidden"
-                accept="image/*"
-            />
-        </>
-    );
+    (async () => {
+      const _res = await patchProfilePicture(authToken);
+      if (_res) {
+          const { fields, url } = await _res.data.postInfo;
+          const formData = new FormData();
+          const formArray: [string, string | File][] = Object.entries({
+              ...fields,
+              file,
+        });
+        formArray.forEach(([key, value]) => {
+          formData.append(key, value);
+        });
+        setFileBlob(URL.createObjectURL(event.target.files[0]));
+        await postProfilePicture(url, formData);
+        successHandler("🎉 Profile picture updated successfully!");
+      }
+    })();
+  };
+  return (
+    <>
+      <i
+        className="absolute bg-white rounded-full p-3 bottom-1 right-0 cursor-pointer shadow-lg"
+        onClick={handleClick}
+      >
+        <EditPencil />
+      </i>
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        onChange={handleChange}
+        className="hidden"
+        accept="image/*"
+      />
+    </>
+  );
 }
