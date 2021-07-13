c77572442e60c1a64b207dcaa849402a3afb7445
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-07-13 18:45:43 +0530

    :bento: Update base url to use env

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 export const authRoutes: string[] = ["/dashboard", "/profile"];
 export const authRestrictedRoutes: string[] = ["/login", "/signup"];
-export const baseUrl: string = "https://links-43b3z.ondigitalocean.app/";
+export const baseUrl: string = process.env.NEXT_PUBLIC_LINKS_BASE_URL;
 export const kzillaxyz: string = "https://kzilla.xyz/analytics/";
 export const kzillaxyzdomain: string = "https://kzilla.xyz/";
 export const kzillaxyzclicks: string = "https://kzilla.xyz/api/v1/analytics/";
